f28df213662b53599a3d5be7f091f9f7565debdb
Guillaume EHRET <guillaume@dev-mind.fr>
2017-03-06 23:33:38 +0100

    Adds redirection to be able to read documents in Google Drive

--- src/main/kotlin/mixit/controller/RedirectController.kt
+++ src/main/kotlin/mixit/controller/RedirectController.kt
@@ -14,7 +14,17 @@ import org.springframework.web.reactive.function.server.ServerRequest
 @Controller
 class RedirectController(val postRepository: PostRepository,
                          val talkRepository: TalkRepository,
-                         @Value("\${baseUri}") val baseUri: String) : RouterFunctionProvider() {
+                         @Value("\${baseUri}") val baseUri: String,
+                         @Value("\${drive.sponsor.form.fr}") val sponsorFormFr: String,
+                         @Value("\${drive.sponsor.form.en}") val sponsorFormEn: String,
+                         @Value("\${drive.sponsor.leaflet.fr}") val sponsorLeafletFr: String,
+                         @Value("\${drive.sponsor.leaflet.en}") val sponsorLeafletEn: String,
+                         @Value("\${drive.speaker.leaflet.fr}") val speakerLeafletFr: String,
+                         @Value("\${drive.speaker.leaflet.en}") val speakerLeafletEn: String,
+                         @Value("\${drive.presse.leaflet.fr}")  val presseLeafletFr: String,
+                         @Value("\${drive.presse.leaflet.en}") val presseLeafletEn: String) : RouterFunctionProvider() {
+
+    val GOOGLE_DRIVE_URI:String = "https://drive.google.com/open"
 
     override val routes: Routes = {
         accept(TEXT_HTML).route {
@@ -23,6 +33,15 @@ class RedirectController(val postRepository: PostRepository,
                 (GET("/{id}") or GET("/{id}/")) { redirectOneArticleView(it) }
             }
             GET("/article/{id}/", this@RedirectController::redirectOneArticleView)
+
+            GET("/docs/sponsor/form/en") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$sponsorFormEn")}
+            GET("/docs/sponsor/form/fr") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$sponsorFormFr")}
+            GET("/docs/sponsor/leaflet/en") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$sponsorLeafletEn")}
+            GET("/docs/sponsor/leaflet/fr") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$sponsorLeafletFr")}
+            GET("/docs/speaker/leaflet/en") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$speakerLeafletEn")}
+            GET("/docs/speaker/leaflet/fr") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$speakerLeafletFr")}
+            GET("/docs/presse/leaflet/en") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$presseLeafletEn")}
+            GET("/docs/presse/leaflet/fr") { permanentRedirect("$GOOGLE_DRIVE_URI?id=$presseLeafletFr")}
 
             GET("/2017/") { permanentRedirect("$baseUri/2017") }
             GET("/2016/") { permanentRedirect("$baseUri/2016") }
